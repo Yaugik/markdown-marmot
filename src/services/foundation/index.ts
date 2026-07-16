8ed0a629e692b2cf520567864d2bd7b659f4c8f8
Prateek Gautam <59331424+the-illuzion@users.noreply.github.com>
2026-07-17 00:26:36 +0530

    Export secure workspace invitation creation

--- src/services/foundation/index.ts
+++ src/services/foundation/index.ts
@@ -1,6 +1,15 @@
 export * from "./errors";
 export * from "./identity";
-export * from "./memberships";
+export { createWorkspaceInvitation } from "./invitations";
+export {
+  acceptWorkspaceInvitation,
+  listWorkspaceInvitations,
+  listWorkspaceMembers,
+  revokeWorkspaceInvitation,
+  updateWorkspaceMember,
+  type WorkspaceInvitation,
+  type WorkspaceMember,
+} from "./memberships";
 export * from "./projects";
 export * from "./queries";
 export * from "./types";
